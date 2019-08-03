116cb21482dc281a353f558f3991192c457179d9
mariacmartins <maria.c.martins07@gmail.com>
2019-08-03 17:41:44 -0300

    first commit

---
+++ project/app_farejando/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Puppy
+
+admin.site.register(Puppy)
---
+++ project/app_farejando/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Puppy
+from .serializers import PuppySerializer
+
+class PuppyView(viewsets.ModelViewSet):
+    queryset = Puppy.objects.all()
+    serializer_class = PuppySerializer
